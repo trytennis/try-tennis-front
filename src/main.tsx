33f88af040a6f5675b09f5a2aea7c8264212baaf
ehkim <ehkim.tech@gmail.com>
2025-09-17 16:14:43 +0900

    feat: 컬러 유틸 CSS 생성 및 main.tsx 등록

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './routers/Router';
 import './styles/index.css';
+import "./styles/Colors.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
